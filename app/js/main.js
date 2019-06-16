81d69b8e0dbadadb1cd714261171229caf3a4151
maksymburiy <001masso@gmail.com>
2019-06-16 19:36:04 +0300

    init tabs on js

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,15 @@
 $(function() {
 
-$(".js-range-slider").ionRangeSlider({
-type: "double",
-min: 0,
-max: 600,
-prefix: "$"
-});
+	if ($(".js-range-slider").length) {
+		$(".js-range-slider").ionRangeSlider({
+			type: "double",
+			min: 0,
+			max: 600,
+			prefix: "$"
+		});		
+	};
 
-$( '.product-tabs' ).tabs();
-
+	if ($(".product-tabs").length) {
+		$( '.product-tabs' ).tabs();
+	}
 });
