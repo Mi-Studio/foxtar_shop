12050adb44c56d6eb9bde8de1f07963e4502de42
Mi-Studio <mitrokhina89@gmail.com>
2019-06-22 12:44:24 +0300

    add slick nav menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,18 @@
 $(function() {
 
-$(".js-range-slider").ionRangeSlider({
-type: "double",
-min: 0,
-max: 600,
-prefix: "$"
-});
+    if($(".js-range-slider").length) {
+        $(".js-range-slider").ionRangeSlider({
+            type: "double",
+            min: 0,
+            max: 600,
+            prefix: "$"
+        });
+    }
+
+    $('.js-slick').slicknav({
+        appendTo: '.menu',
+        label:''
+    });
+
 
 });
